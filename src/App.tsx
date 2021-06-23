d9f14fa8c0a649f0aa69c34da80c47af052b953c
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-06-23 14:57:41 -0300

    Roteamento e navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-//import { Home } from "./pages/Home";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    //<Home />
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,7 @@
-import illustrationImg from '../assets/images/illustration.svg'
+import { Link } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
@@ -29,7 +30,7 @@ export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
                     </p>
                 </div>
             </main>
