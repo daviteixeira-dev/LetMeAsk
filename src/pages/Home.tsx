8aa07680048ceb2988afc9f1d530aaa3d76b0933
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-06-23 22:22:39 -0300

    Contexto de autenticação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useHistory } from 'react-router-dom';
-import { auth, firebase } from '../services/firebase';
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -7,22 +6,20 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
 
 
 export function Home() {
 
     const history = useHistory();
+    const { user, signInWithGoogle } = useContext(AuthContext);
 
-    function handleCreateRoom() {
-
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result);
-            history.push('/rooms/new');
-        });
-
-        
+    async function handleCreateRoom(){
+        if(!user){
+            await signInWithGoogle();
+        }
+        history.push('/rooms/new');
     }
 
     return (
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -9,6 +11,9 @@ import '../styles/auth.scss';
 
 
 export function NewRoom() {
+
+    const { user } = useContext(AuthContext);
+
     return (
         <div id="page-auth">
             <aside>
@@ -19,6 +24,7 @@ export function NewRoom() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
+                    <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
                     <form>
                         <input
