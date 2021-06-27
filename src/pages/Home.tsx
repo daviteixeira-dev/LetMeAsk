c42fc86497932c65bc36014b46f394b0ec5f208a
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-06-27 14:16:06 -0300

    Funcionalidade de encerrar sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,6 +37,11 @@ export function Home() {
            return;
        }
 
+       if(roomRef.val().endedAt){
+           alert('Room does not exists.');
+           return;
+       }
+
        history.push(`/rooms/${roomCode}`);
     }
 
